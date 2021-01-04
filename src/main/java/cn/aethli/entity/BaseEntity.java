ca0ef09c35cb6ca508b5cde9b1e073ce6291a952
yangrenjie <selcarpa@gmail.com>
2021-01-04 16:24:36 +0800

    initial commit

---
+++ src/main/java/cn/aethli/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package cn.aethli.entity;
+
+public abstract class BaseEntity {
+  /** primary key */
+  protected String id;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+}
